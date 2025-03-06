12caf1770690b67ba0b9bf26e81022bc165d5df7
Vedraj Gawas <gawasvedraj@gmail.com>
2025-03-06 21:49:01 +0530

    gemstone: Don't include AudioFX
    Bloat, since nobody uses it.

--- lineage_gemstone.mk
+++ lineage_gemstone.mk
@@ -15,6 +15,8 @@ $(call inherit-product, device/xiaomi/gemstone/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := gemstone
